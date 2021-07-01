86a3c6ed78ffecfa3746adfd6ef1a43ef714056a
BigCubeCat <>
2021-07-01 20:50:31 +0700

    fix load image, add save function

--- app.py
+++ app.py
@@ -16,36 +16,47 @@ class BearFinderApp(QMainWindow, Ui_BearFinder):
         super().setupUi(self)
         self.find_function = None
         self.image_size = 0, 0,
-        self.border_color = "#AA0000"
+        self.border_color = "#68e4b2"
         self.detector_acive = False
         self.model_name = model_name
         self.filename = None
         self.work_directory = "~/"
+        self.save_filename = None
         self.initUi()
     
     def set_image(self):
-        im = self.run_finder()
-        print(im)
-        pix = QPixmap.fromImage(im)
-        print("ok")
+        qim = self.run_finder()
+        pix = QPixmap.fromImage(qim)
+        pix.detach()
         self.canvas.setPixmap(pix)
-        print("ok")
-        self.canvas.update()
-        print("u")
+        self.infoText.setText(self.getInfoText())
+    
+    def save(self):
+        if self.save_filename is None:
+            self.save_as()
+        self.canvas.pixmap().save(self.save_filename)
+    
+    def save_as(self):
+        self.save_filename = QFileDialog.getSaveFileName(
+            self, 'Save As', '', 'All files (*.*);;PNG (*.png);;JPEG(*.jpg)'
+        )[0]
+        self.save()
+                                        
 
     def run_finder(self):
-        image = Image.open(self.filename) # TODO: Replace to call detector
+        image = Image.open(self.filename) 
         self.image_size = image.size
         if not self.detector_acive:
             init(self.model_name)
             self.detector_acive = True
         image = image_pillow(self.filename, color=self.border_color)
-        image.show()
         return ImageQt.ImageQt(image)
     
     def initUi(self):
         self.setWindowTitle("Polar Bear finder")
 
+        self.actionSaveAs.triggered.connect(self.save_as)
+        self.actionSave.triggered.connect(self.save)
         self.actionQuit.triggered.connect(self.close)
         self.scrollArea.setWidget(self.canvas)
         self.actionOpen.triggered.connect(self.openAskFileDialog)
@@ -56,24 +67,25 @@ class BearFinderApp(QMainWindow, Ui_BearFinder):
         
         self.colorButton.clicked.connect(self.chooseColor)
         self.infoText.setText(self.getInfoText())
+        self.colorButton.setStyleSheet("QWidget { background-color: %s }" % self.border_color)
     
     def chooseColor(self):
         col = QColorDialog.getColor()
         if col.isValid():
-            self.colorButton.setStyleSheet("QWidget { background-color: %s }" % col.name())
             self.border_color = col.name()
+            self.colorButton.setStyleSheet("QWidget { background-color: %s }" % self.border_color)
     
     def getInfoText(self):
         """
         return text to discription panel.
         """
-        return f"""Discription\n\nImage:       {self.filename}\n\nFolder:      {self.work_directory}\n\nImage size:  {self.border_color}"""
+        return f"""Discription\n\nImage:       {self.filename}\n\nFolder:      {self.work_directory}\n\nImage size:  {self.image_size}"""
     
     def openAskFileDialog(self):
         self.filename = QFileDialog.getOpenFileName(self,
             "Open Image", 
             QDir.currentPath(),
-            "Image Files (*.png *.jpg *.jpeg)"
+            'All files (*.*);;PNG (*.png);;JPEG(*.jpg)'
         )[0]
         self.work_directory = os.path.dirname(self.filename)
         QThreadPool.globalInstance().start(self.set_image)
